2df8e41ed7209ffe14791d35831939ee3514e7c4
surik72 <91281832+surik72@users.noreply.github.com>
2025-01-09 07:45:02 +0530

    Add files via upload
    Simplewebsite

---
+++ script.js
@@ -0,0 +1,8 @@
+// Adding interactivity to the buttons
+document.getElementById('getStarted').addEventListener('click', () => {
+    alert('Getting Started! This will navigate you to the tutorial.');
+});
+
+document.getElementById('subscribe').addEventListener('click', () => {
+    alert('Thank you for subscribing to our website!');
+});
